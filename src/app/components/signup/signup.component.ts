1d0307a612b4a3a61fca8c3373fd47cae723f236
PraveenShaun <praveen18.warner@gmail.com>
2023-07-01 00:46:17 +0530

    created common scss

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  passwordType: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa-eye-slash';
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  hideShowPassword() {
+    this.isText = !this.isText;
+    this.isText ? (this.eyeIcon = 'fa-eye') : (this.eyeIcon = 'fa-eye-slash');
+    this.isText
+      ? (this.passwordType = 'text')
+      : (this.passwordType = 'password');
+  }
 }
